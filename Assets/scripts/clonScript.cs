3616b90053aa7d0ae99601fcf2a78be1729570be
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-04-16 21:55:42 +0430

    update clone script

--- Assets/scripts/clonScript.cs
+++ Assets/scripts/clonScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class clonScript : MonoBehaviour
 {
+    public GameObject apple;
+    public int timer=0;
+    private float spawn;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(apple, new Vector2(spawn, transform.position.y), apple.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        spawn = Random.Range(-9, 9);
+        increseTime();
+       
         
+    }
+    public void increseTime()
+    {
+        if (timer > 72)
+        {
+            Instantiate(apple, new Vector2(spawn, transform.position.y), apple.transform.rotation);
+            timer = 0;
+        }
+        timer++;
     }
 }
